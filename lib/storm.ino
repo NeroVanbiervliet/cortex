e1b067b7a53e2437e4a1ab61d14f04143501c20d
Nero Vanbiervliet <louis.vanbiervliet@gmail.com>
2018-10-12 17:26:14 +0200

    switch from delay to msTimer2 in storm lib

--- lib/storm.ino
+++ lib/storm.ino
@@ -1,13 +1,24 @@
+#include <MsTimer2.h>
+
 #define TIME_STROBE_ON 5000 // milliseconds
+
 
 void stormSetup() {
     pinMode(RELAIS_STROBOSCOPE, OUTPUT); 
     digitalWrite(RELAIS_STROBOSCOPE, LOW);
+    MsTimer2::set(TIME_STROBE_ON, endStorm);
 }
 
 void performStorm(boolean silent=false) {
+  Serial.println("performing Storm"); 
   if (!silent) makeSound(SND_STORM); 
   digitalWrite(RELAIS_STROBOSCOPE, HIGH); 
-  delay(TIME_STROBE_ON); // TODO blocks program, use timer2
+
+  // timer will end the storm
+  MsTimer2::start();
+}
+
+void endStorm() {
+  MsTimer2::stop();
   digitalWrite(RELAIS_STROBOSCOPE, LOW); 
 }
